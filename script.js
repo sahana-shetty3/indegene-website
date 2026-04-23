c6df66099aa3f94c50bcac4e57bde6f906e202a6
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-04-23 20:13:31 +0530

    updated

--- script.js
+++ script.js
@@ -40,3 +40,23 @@ menuLinks.forEach(link => {
     });
 });
 
+const sections = document.querySelectorAll("section");
+const navLinks=document.querySelectorAll(".side-menu a");
+
+window .addEventListener("scroll",()=>{
+    let current ="";
+
+    sections.forEach(section =>{
+        const sectionTop = section.offsetTop=100;
+        if(scrollY >= sectionTop){
+            current = section.getAttribute("id");
+        }
+    });
+    navLinks.forEach(link =>{
+        link.classList.remove("active");
+        if(link.getAttribute("href")==="#"+current){
+            link.classList.add("active");
+        }
+    });
+});
+
